6a4058f0f11b28e333955712cb445ec5088a060c
Stephen Thirlwall <stephent@logicly.com.au>
2022-12-03 22:48:09 +1100

    Day 03 part 2

--- day03/day03.go
+++ day03/day03.go
@@ -10,6 +10,7 @@ func main() {
 	lines := aoc.GetInputLines(filename)
 
 	fmt.Println(part1(lines))
+	fmt.Println(part2(lines))
 }
 
 func part1(lines []string) int {
@@ -17,6 +18,14 @@ func part1(lines []string) int {
 	for _, line := range lines {
         total += getPriorities(line)
 	}
+	return total
+}
+
+func part2(lines []string) int {
+    total := 0
+    for i := 0; i < len(lines); i += 3 {
+        total += getCommon(lines[i:i+3])
+    }
 	return total
 }
 
@@ -34,6 +43,28 @@ func getPriorities(rucksack string) int {
         value := getValue(item)
         if seen[value] > 0 {
             return value + 1
+        }
+    }
+    return 0
+}
+
+func getCommon(lines []string) int {
+    seen := make([]int, 52)
+
+    all := 0
+    for i, line := range lines {
+        bit := 1 << i
+        all |= bit
+
+        for _, item := range line {
+            value := getValue(byte(item))
+            seen[value] |= bit
+        }
+    }
+
+    for i, mask := range seen {
+        if mask == all {
+            return i + 1
         }
     }
     return 0

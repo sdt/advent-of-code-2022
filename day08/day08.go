cfcafa3aa19c9dbb129bfd438e5c0975032a3db1
Stephen Thirlwall <stephent@logicly.com.au>
2022-12-08 21:01:33 +1100

    Day 08 part 2

--- day08/day08.go
+++ day08/day08.go
@@ -15,6 +15,7 @@ func main() {
     lines := aoc.GetInputLines(filename)
 
     fmt.Println(part1(lines))
+    fmt.Println(part2(lines))
 }
 
 func part1(lines[] string) int {
@@ -34,10 +35,54 @@ func part1(lines[] string) int {
     return numVisible
 }
 
+func part2(lines[] string) int {
+    treeSize := ParseTrees(lines)
+
+    best := -1
+    for y := 0; y < treeSize.h; y++ {
+        for x := 0; x < treeSize.w; x++ {
+            score := computeScenicScore(treeSize, x, y)
+            if score > best {
+                best = score
+            }
+        }
+    }
+    return best
+}
+
+func computeScenicScore(treeSize Grid[int], x, y int) int {
+    u := computeSingleScore(treeSize, x, y,  0, -1)
+    l := computeSingleScore(treeSize, x, y, -1,  0)
+    r := computeSingleScore(treeSize, x, y,  1,  0)
+    d := computeSingleScore(treeSize, x, y,  0,  1)
+    score := u * l * r * d
+    //fmt.Printf("%d,%d(%d) %d * %d * %d * %d = %d\n", x, y, treeSize.Get(x, y), u, l, r, d, score)
+    return score
+}
+
+func computeSingleScore(treeSize Grid[int], x, y, dx, dy int) int {
+    limit := treeSize.Get(x, y)
+    count := 0
+    for {
+        x += dx
+        y += dy
+        if !treeSize.OnGrid(x, y) {
+            return count
+        }
+        h := treeSize.Get(x, y)
+        if h >= limit {
+            return count+1
+        }
+        count++
+    }
+    return count
+}
+
+
 func markVisible(treeSize Grid[int], visible *Grid[bool], x, y, dx, dy int) int {
     max := -1
     count := 0
-    for treeSize.onGrid(x, y) {
+    for treeSize.OnGrid(x, y) {
         h := treeSize.Get(x, y)
         if h <= max {
             //fmt.Printf("%d,%d is not visible (%d -> %d)\n", x, y, max, h)
@@ -84,7 +129,7 @@ func (this *Grid[T]) Get(x, y int) T {
     return this.cell[this.offset(x, y)]
 }
 
-func (this Grid[T]) onGrid(x, y int) bool {
+func (this Grid[T]) OnGrid(x, y int) bool {
     return (x >= 0) && (y >= 0) && (x < this.w) && (y < this.h)
 }
 

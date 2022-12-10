297f6cc7b5aca739e90521f37d9a4506266d7057
Stephen Thirlwall <stephent@logicly.com.au>
2022-12-10 19:51:42 +1100

    Day 10 part 2

--- day10/day10.go
+++ day10/day10.go
@@ -17,6 +17,7 @@ func main() {
     lines := aoc.GetInputLines(filename)
 
     fmt.Println(part1(lines))
+    part2(lines)
 }
 
 func part1(lines []string) int {
@@ -47,6 +48,42 @@ func part1(lines []string) int {
     return signalStrength
 }
 
+func part2(lines []string) {
+    vm := NewVM()
+
+    for _, line := range lines {
+        incr := 0
+        cycles := 1
+
+        tokens := strings.Split(line, " ")
+        if tokens[0] == "addx" {
+            incr = aoc.ParseInt(tokens[1])
+            cycles = 2
+        }
+
+        for i := 0; i < cycles; i++ {
+            if Abs(vm.x - vm.cycle) <= 1 {
+                fmt.Print("#")
+            } else {
+                fmt.Print(".")
+            }
+            vm.cycle++
+            if vm.cycle == 40 {
+                fmt.Println()
+                vm.cycle = 0
+            }
+        }
+        vm.x += incr
+    }
+}
+
 func NewVM(signalCycles ...int) VM {
     return VM{ x: 1, cycle: 0, signalCycles: signalCycles }
+}
+
+func Abs(x int) int {
+    if x < 0 {
+        return -x
+    }
+    return x
 }

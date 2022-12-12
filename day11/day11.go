1f64ca368975d869d7684d05f7e7e446404e7e2a
Stephen Thirlwall <stephent@logicly.com.au>
2022-12-12 12:56:20 +1100

    Day 11 part 2
    :phew:

--- day11/day11.go
+++ day11/day11.go
@@ -13,6 +13,8 @@ type Op func(int) int
 type Monkey struct {
     items []int
     op Op
+    mod int
+    div int
     test int
     trueMonkey, falseMonkey int
     inspections int
@@ -22,16 +24,48 @@ func main() {
     filename := aoc.GetFilename()
     lines := aoc.GetInputLines(filename)
     fmt.Println(part1(lines))
+    fmt.Println(part2(lines))
 }
 
 func part1(lines []string) int {
     monkeys := make([]Monkey, 0)
 
     for i := 0; i < len(lines); i += 7 {
-        monkeys = append(monkeys, parseMonkey(lines[i:]))
+        monkey := parseMonkey(lines[i:])
+        monkey.div = 3
+        monkeys = append(monkeys, monkey)
     }
 
     for i := 0; i < 20; i++ {
+        doRound(monkeys)
+    }
+
+    inspections := make([]int, len(monkeys))
+    for i := range(monkeys) {
+        inspections[i] = monkeys[i].inspections
+    }
+    sort.Sort(sort.Reverse(sort.IntSlice(inspections)))
+
+    return inspections[0] * inspections[1]
+}
+
+func part2(lines []string) int {
+    monkeys := make([]Monkey, 0)
+
+    for i := 0; i < len(lines); i += 7 {
+        monkey := parseMonkey(lines[i:])
+        monkeys = append(monkeys, monkey)
+    }
+
+    totalMod := 1
+    for i := range monkeys {
+        totalMod *= monkeys[i].test
+    }
+    for i := range monkeys {
+        monkeys[i].mod = totalMod
+    }
+
+    for i := 0; i < 10000; i++ {
         doRound(monkeys)
     }
 
@@ -56,7 +90,7 @@ func doRound(monkeys []Monkey) {
 }
 
 func (this Monkey) doItem(item int) (int, int) {
-    item = this.op(item) / 3
+    item = this.op(item % this.mod) / this.div
 
     if int(item) % this.test == 0 {
         return item, this.trueMonkey
@@ -68,6 +102,8 @@ func parseMonkey(lines []string) Monkey {
     return Monkey{
         items: parseItems(lines[1]),
         op: parseOp(lines[2]),
+        mod: 10000000000,
+        div: 1,
         test: parseInt(lines[3], "  Test: divisible by "),
         trueMonkey: parseInt(lines[4], "    If true: throw to monkey "),
         falseMonkey: parseInt(lines[5], "    If false: throw to monkey "),

43881eade1289fd682c97052307c80632b621683
Stephen Thirlwall <stephent@logicly.com.au>
2022-12-13 16:59:49 +1100

    Day 13 part 1

---
+++ day13/day13.go
@@ -0,0 +1,169 @@
+package main
+
+import (
+	"advent-of-code/aoc"
+	"fmt"
+)
+
+type ValueType int
+const (
+    IntValue  ValueType = iota
+    ListValue
+)
+
+type Comparison int
+const (
+    RightOrder Comparison = iota
+    WrongOrder
+    Indeterminate
+)
+
+type PacketValue struct {
+    valueType   ValueType
+    intValue    int
+    listValue   []*PacketValue
+}
+
+func main() {
+	filename := aoc.GetFilename()
+	lines := aoc.GetInputLines(filename)
+	fmt.Println(part1(lines))
+}
+
+func part1(lines []string) int {
+    pair := 1
+    result := 0
+    for {
+        left := parseLine(lines[0])
+        right := parseLine(lines[1])
+        comp := compare(*left, *right)
+        //fmt.Println(pair, left, right, comp)
+
+        if comp == RightOrder {
+            result += pair
+        }
+
+        if len(lines) == 2 {
+            break;
+        }
+        lines = lines[3:]
+        pair++
+    }
+	return result
+}
+
+func parseLine(line string) *PacketValue {
+    rest, value := parse(line)
+    if len(rest) != 0 {
+        panic("Parse error")
+    }
+    return value
+}
+
+func parse(line string) (string, *PacketValue) {
+    //fmt.Printf("parse('%s')\n", line)
+    if line[0] == '[' {
+        rest, listValue := parseListValue(line[1:])
+        return rest, &PacketValue{
+            valueType: ListValue,
+            listValue: listValue,
+        }
+    }
+    rest, intValue := parseIntValue(line)
+    return rest, &PacketValue{
+        valueType: IntValue,
+        intValue:  intValue,
+    }
+}
+
+func parseListValue(line string) (string, []*PacketValue) {
+    //fmt.Printf("parseList('%s')\n", line)
+    values := make([]*PacketValue, 0)
+    for line[0] != ']' {
+        next, value := parse(line)
+        values = append(values, value)
+        if next[0] == ',' {
+            next = next[1:]
+        }
+        line = next
+    }
+    return line[1:], values
+}
+
+func parseIntValue(line string) (string, int) {
+    //fmt.Printf("parseInt('%s') -> ", line)
+    value := 0
+    for line[0] >= '0' && line[0] <= '9' {
+        value = value * 10 + int(line[0] - '0')
+        line = line[1:]
+    }
+    //fmt.Printf("['%s', %d]\n", line, value)
+    return line, value
+}
+
+func (this PacketValue) String() string {
+    if this.valueType == IntValue {
+        return fmt.Sprintf("%d", this.intValue)
+    }
+
+    s := "["
+    for i, v := range this.listValue {
+        if i != 0 {
+            s += ","
+        }
+        s += v.String()
+    }
+    s += "]"
+    return s
+}
+
+func (this PacketValue) MakeListValue() PacketValue {
+    if this.valueType == ListValue {
+        return this
+    }
+
+    return PacketValue{
+        valueType: ListValue,
+        listValue: []*PacketValue{ &this },
+    }
+}
+
+func compare(left PacketValue, right PacketValue) Comparison {
+    if left.valueType == IntValue && right.valueType == IntValue {
+        switch {
+            case left.intValue < right.intValue:
+                return RightOrder
+
+            case left.intValue > right.intValue:
+                return WrongOrder
+
+            default:
+                return Indeterminate
+        }
+    }
+
+    if left.valueType == ListValue && right.valueType == ListValue {
+        leftVal := left.listValue
+        rightVal := right.listValue
+
+        for {
+            if len(leftVal) == 0 {
+                if len(rightVal) == 0 {
+                    return Indeterminate
+                }
+                return RightOrder
+            }
+            if len(rightVal) == 0 {
+                return WrongOrder
+            }
+            comp := compare(*leftVal[0], *rightVal[0])
+            if comp != Indeterminate {
+                return comp
+            }
+            leftVal = leftVal[1:]
+            rightVal = rightVal[1:]
+        }
+    }
+
+    return compare(left.MakeListValue(), right.MakeListValue())
+}

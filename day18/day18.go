fee906d9434224f52f16061831c9cccb0ea1b42c
Stephen Thirlwall <sdt@dr.com>
2022-12-19 08:50:05 +1100

    Simplify day 18 part 1

--- day18/day18.go
+++ day18/day18.go
@@ -3,34 +3,11 @@ package main
 import (
 	"advent-of-code/aoc"
 	"fmt"
-	"sort"
+	"math"
 	"strings"
 )
 
-type Side uint8
-
-// RH coords.
-const (
-	East     = 1 << iota // +x
-	West     = 1 << iota // -x
-	North    = 1 << iota // +y
-	South    = 1 << iota // -y
-	Up       = 1 << iota // +Z
-	Down     = 1 << iota // -Z
-
-	XMask    = East | West
-	YMask    = North | South
-	ZMask    = Up | Down
-
-	All		 = XMask | YMask | ZMask
-)
-
 type Vec3 [3]int
-
-type Cube struct {
-	coords	  Vec3
-	openSides Side
-}
 
 type BBox struct {
 	min Vec3
@@ -41,26 +18,38 @@ func main() {
 	filename := aoc.GetFilename()
 	lines := aoc.GetInputLines(filename)
 
-	cubes := make([]*Cube, len(lines))
-	for i, line := range lines {
-		cubes[i] = parseCube(line)
-	}
-
-	fmt.Println(part1(cubes))
-	fmt.Println(part2(cubes))
-}
-
-func part1(cubes []*Cube) int {
-	return findSurfaceArea(cubes)
-}
-
-func part2(cubes []*Cube) int {
 	cubeMap := make(map[Vec3]bool)
-	for _, cube := range cubes {
-		cubeMap[cube.coords] = true
+	for _, line := range lines {
+		cube := parseCube(line)
+		cubeMap[cube] = true
 	}
 
-	volume := makeVolume(cubes)
+	fmt.Println(part1(cubeMap))
+	fmt.Println(part2(cubeMap))
+}
+
+func part1(cubeMap map[Vec3]bool) int {
+	surfaceArea := 0
+	for pos := range cubeMap {
+		for axis := range pos {
+			before := pos
+			before[axis]--
+			if !cubeMap[before] {
+				surfaceArea++
+			}
+
+			after := pos
+			after[axis]++
+			if !cubeMap[after] {
+				surfaceArea++
+			}
+		}
+	}
+	return surfaceArea
+}
+
+func part2(cubeMap map[Vec3]bool) int {
+	volume := makeVolume(cubeMap)
 	seen := make(map[Vec3]bool)
 	surfaceArea := 0
 
@@ -107,41 +96,10 @@ func part2(cubes []*Cube) int {
 	return surfaceArea
 }
 
-func findSurfaceArea(cubes []*Cube) int {
-	for axis := 0; axis < 3; axis++ {
-		matchCubes(cubes, axis)
-	}
-
-	area := 0
-	for _, cube := range(cubes) {
-		area += cube.SurfaceArea()
-	}
-
-	return area
-}
-
-func matchCubes(cubes []*Cube, axis int) {
-	sort.Slice(cubes, func(a, b int) bool {
-		return cubes[a].coords[axis] < cubes[b].coords[axis]
-	})
-
-	for i, a := range cubes[:len(cubes)-1] {
-		for _, b := range cubes[i+1:] {
-			if isAdjacent(a, b, axis) {
-				aSide := Side(1 << (axis * 2))
-				bSide := Side(aSide << 1)
-
-				a.openSides &= ^aSide
-				b.openSides &= ^bSide
-			}
-		}
-	}
-}
-
-func makeVolume(cubes []*Cube) BBox {
-	bbox := newBBox(*cubes[0])
-	for _, cube := range cubes[1:] {
-		bbox = bbox.extend(*cube)
+func makeVolume(cubes map[Vec3]bool) BBox {
+	bbox := newBBox()
+	for cube := range cubes {
+		bbox = bbox.extend(cube)
 	}
 
 	for i := range bbox.min {
@@ -152,53 +110,26 @@ func makeVolume(cubes []*Cube) BBox {
 	return bbox
 }
 
-func isAdjacent(a, b *Cube, axis int) bool {
-	for c := 0; c < 3; c++ {
-		if c == axis {
-			if a.coords[c] != b.coords[c] - 1 {
-				return false
-			}
-		} else {
-			if a.coords[c] != b.coords[c] {
-				return false
-			}
-		}
-	}
-	return true
-}
-
-func parseCube(line string) *Cube {
+func parseCube(line string) Vec3 {
 	coords := aoc.ParseInts(strings.Split(line, ","))
-	cube := Cube{ openSides: All }
-	copy(cube.coords[:], coords)
-	return &cube
+	var p Vec3
+	copy(p[:], coords)
+	return p
 }
 
-func (this Cube) SurfaceArea() int {
-	area := 0
-	for i := 0; i < 6; i++ {
-		if this.openSides & (1 << i) != 0 {
-			area++
-		}
-	}
-	return area
+func newBBox() BBox {
+	min := [3]int{ math.MaxInt, math.MaxInt, math.MaxInt }
+	max := [3]int{ math.MinInt, math.MinInt, math.MinInt }
+	return BBox{ min, max }
 }
 
-func newBBox(cube Cube) BBox {
-	return BBox{ cube.coords, cube.coords }
-}
-
-func (this BBox) extend(cube Cube) BBox {
+func (this BBox) extend(p Vec3) BBox {
 	var bbox BBox
-	for axis, value := range cube.coords {
+	for axis, value := range p {
 		bbox.min[axis] = min(this.min[axis], value)
 		bbox.max[axis] = max(this.max[axis], value)
 	}
 	return bbox
-}
-
-func (this BBox) size(axis int) int {
-	return this.max[axis] - this.min[axis] + 1
 }
 
 func (this BBox) contains(p Vec3) bool {
